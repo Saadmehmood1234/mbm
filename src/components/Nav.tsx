b31926441585cd32fe8a04c2dd55ff997b1a50eb
Saadmehmood1234 <158064428+Saadmehmood1234@users.noreply.github.com>
2025-03-21 01:44:01 +0530

    added

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -18,16 +18,18 @@ const Nav = async () => {
   const user = session.user;
   console.log("Session", session.user);
   return (
-    <header className="w-full flex justify-between items-center h-20 bg-white shadow-md px-8">
-      <div className="flex items-center gap-2">
-        <Rocket className="w-8 h-8 text-blue-600" />
-        <span className="text-xl font-bold text-gray-800">Sonu Don</span>
-      </div>
+    <header className="w-full border-b-4 border-amber-400 flex justify-between items-center h-20 bg-gradient-to-br from-purple-800 to-indigo-900 shadow-md px-8">
+      <Link href="/">
+        <div className="flex items-center gap-2">
+          <Rocket className="w-8 h-8 text-amber-400" />
+          <span className="text-xl font-bold text-gray-200">Sonu Don</span>
+        </div>
+      </Link>
       <div className="flex items-center gap-6">
         <Link href="/dashboard">
           {" "}
-          <button className="p-2 rounded-lg hover:bg-gray-100 transition-colors">
-            <LayoutDashboard className="w-6 h-6 text-gray-600" />
+          <button className="p-2 rounded-lg cursor-pointer transition-colors">
+            <LayoutDashboard className="w-6 h-6 text-amber-400 hover:text-amber-200" />
           </button>
         </Link>
 
@@ -39,10 +41,10 @@ const Nav = async () => {
               className="w-10 h-10 rounded-full"
             />
           ) : (
-            <UserCircle className="w-8 h-8 text-gray-600" />
+            <UserCircle className="w-8 h-8 text-amber-400" />
           )}
           <div className="flex items-center gap-1">
-            <span className="text-gray-700 font-medium">
+            <span className="text-amber-400 font-medium">
               {session.user?.name || "Account"}
             </span>
             {/* <ChevronDown className="w-4 h-4 text-gray-500 group-hover:text-gray-700 transition-colors" /> */}
